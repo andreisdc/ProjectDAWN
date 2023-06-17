2f20e43aa6f46d3c53fc9f992512b95253d26c1c
Alexandru Edves <95491924+AlexE10@users.noreply.github.com>
2023-06-17 16:03:09 +0300

    Created fragments.

--- app/src/main/java/com/pontic_studio/myproperty/MainActivity/MainActivity.java
+++ app/src/main/java/com/pontic_studio/myproperty/MainActivity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.pontic_studio.myproperty;
+package com.pontic_studio.myproperty.MainActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.pontic_studio.myproperty.R;
 
 public class MainActivity extends AppCompatActivity {
 
