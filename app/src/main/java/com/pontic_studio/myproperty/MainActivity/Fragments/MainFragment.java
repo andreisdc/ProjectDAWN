5e76555ecd6171ba6daa3d6b113745adb8c3847a
Andrei Samulescu <andy_sdc2002@yahoo.com>
2023-06-17 17:13:55 +0300

    Attached Navigation

--- app/src/main/java/com/pontic_studio/myproperty/MainActivity/Fragments/MainFragment.java
+++ app/src/main/java/com/pontic_studio/myproperty/MainActivity/Fragments/MainFragment.java
@@ -10,11 +10,7 @@ import android.view.ViewGroup;
 
 import com.pontic_studio.myproperty.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class MainFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
