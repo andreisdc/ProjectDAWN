520be7cd37d4c90bceb298cabb98c63c62ab11e2
Alexandru Edves <95491924+AlexE10@users.noreply.github.com>
2023-06-18 18:40:08 +0300

    Created activitie for client, admin and owner

---
+++ app/src/main/java/com/pontic_studio/myproperty/AdminActivity/AdminActivity.java
@@ -0,0 +1,16 @@
+package com.pontic_studio.myproperty.AdminActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pontic_studio.myproperty.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
